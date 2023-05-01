bbd2de8a5eadb74c7b60dca489d0ce02a0c8314b
vincent-pli <justdoit.pli@gmail.com>
2023-05-01 08:54:51 +0800

    fix incorrect usage for arg bool

--- evaporate/run.sh
+++ evaporate/run.sh
@@ -3,21 +3,20 @@ keys="PLACEHOLDER" # INSERT YOUR API KEY(S) HERE
 # evaporate code closed ie
 python run_profiler.py \
     --data_lake fda_510ks \
-    --do_end_to_end False \
     --num_attr_to_cascade 50 \
     --num_top_k_scripts 10 \
     --train_size 10 \
     --combiner_mode ws \
-    --use_dynamic_backoff True \
+    --use_dynamic_backoff \
     --KEYS ${keys}
 
 # evaporate code open ie
 python run_profiler.py \
     --data_lake fda_510ks \
-    --do_end_to_end True \
+    --do_end_to_end \
     --num_attr_to_cascade 1 \
     --num_top_k_scripts 10 \
     --train_size 10 \
     --combiner_mode ws \
-    --use_dynamic_backoff True \
+    --use_dynamic_backoff \
     --KEYS ${keys}
